869899aaa8eb5d2b4df3460af6753107bd0b2b38
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-07-10 23:28:07 +0200

    Added SingltonMono

---
+++ COSMOS/Assets/Scripts/Core/SingltonMono.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace COSMOS.Core
+{
+    public class SingltonMono<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T Instance { get; private set; }
+
+        protected bool Init()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+                return true;
+            }
+            return false;
+        }
+    }
+}
