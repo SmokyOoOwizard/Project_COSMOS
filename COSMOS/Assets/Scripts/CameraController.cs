be626bd6474d54cb2fd65790c2d888107082ac45
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-08-03 01:12:12 +0200

    Fix CameraController

--- COSMOS/Assets/Scripts/CameraController.cs
+++ COSMOS/Assets/Scripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
     public float Radius;
     public float Height = 2f;
     public float Distance = 1;
-    public bool DrawDebug = false;
+    public bool DrawDebug = true;
 
     void Update()
     {
@@ -27,13 +27,12 @@ public class CameraController : MonoBehaviour
         NewPos.x = Mathf.Sin(Rot.y * Mathf.Deg2Rad) * -radius;
         Rot.x = Mathf.Atan(height / radius) * Mathf.Rad2Deg;
         if (float.IsNaN(Rot.x)) Rot.x = 0;
+        if (float.IsNaN(Rot.z)) Rot.z = 0;
         transform.rotation = Quaternion.Euler(Rot.x, Rot.y, Rot.z);
         if (Target != null)
         {
             Vector3 newPos = new Vector3(0, NewPos.y + Target.position.y, 0);
-            //newPos.z = Mathf.Lerp(transform.position.z, NewPos.z + Target.transform.position.z, Time.deltaTime * Speed);
             newPos.z = -NewPos.z + Target.position.z;
-            //newPos.x = Mathf.Lerp(transform.position.x, NewPos.x + Target.transform.position.x, Time.deltaTime * Speed);
             newPos.x = NewPos.x + Target.position.x;
 
             transform.position = NewPos + Target.position;
@@ -43,30 +42,36 @@ public class CameraController : MonoBehaviour
             transform.position = NewPos + Target.position;
         }
     }
-    private void OnDrawGizmos()
+    private void OnDrawGizmosSelected()
     {
         if (this.enabled)
         {
             SetCameraPos();
-        }
-        if (DrawDebug)
-        {
-            Gizmos.color = Color.red;
+            if (DrawDebug)
+            {
+                Gizmos.color = Color.red;
 
-            Gizmos.DrawLine(new Vector3(Target.position.x + Radius, Target.position.y, transform.position.z), new Vector3(Target.position.x + Radius, Target.position.y, Target.position.z));
-            Gizmos.DrawLine(new Vector3(Target.position.x + Radius, transform.position.y, transform.position.z), new Vector3(Target.position.x + Radius, Target.position.y, Target.position.z));
-            Gizmos.DrawLine(new Vector3(Target.position.x + Radius, transform.position.y, transform.position.z), new Vector3(Target.position.x + Radius, Target.position.y, transform.position.z));
+                Vector3 targetPos = Vector3.zero;
+                if (Target != null)
+                {
+                    targetPos = Target.position;
+                }
 
-            Gizmos.DrawLine(new Vector3(Target.position.x + Radius, transform.position.y, transform.position.z), new Vector3(transform.position.x, transform.position.y, transform.position.z));
-            Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(transform.position.x, Target.position.y, transform.position.z));
+                Gizmos.DrawLine(new Vector3(targetPos.x + Radius, targetPos.y, transform.position.z), new Vector3(targetPos.x + Radius, targetPos.y, targetPos.z));
+                Gizmos.DrawLine(new Vector3(targetPos.x + Radius, transform.position.y, transform.position.z), new Vector3(targetPos.x + Radius, targetPos.y, targetPos.z));
+                Gizmos.DrawLine(new Vector3(targetPos.x + Radius, transform.position.y, transform.position.z), new Vector3(targetPos.x + Radius, targetPos.y, transform.position.z));
 
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(transform.position, Target.position);
+                Gizmos.DrawLine(new Vector3(targetPos.x + Radius, transform.position.y, transform.position.z), new Vector3(transform.position.x, transform.position.y, transform.position.z));
+                Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(transform.position.x, targetPos.y, transform.position.z));
 
-            Gizmos.color = Color.yellow;
-            Gizmos.matrix = Matrix4x4.Scale(new Vector3(1, 0, 1));
-            Gizmos.DrawWireSphere(Target.position, Radius);
-            Gizmos.matrix = Matrix4x4.identity;
+                Gizmos.color = Color.green;
+                Gizmos.DrawLine(transform.position, targetPos);
+
+                Gizmos.color = Color.yellow;
+                Gizmos.matrix = Matrix4x4.Scale(new Vector3(1, 0, 1));
+                Gizmos.DrawWireSphere(targetPos, Radius * Distance);
+                Gizmos.matrix = Matrix4x4.identity;
+            }
         }
     }
 }
