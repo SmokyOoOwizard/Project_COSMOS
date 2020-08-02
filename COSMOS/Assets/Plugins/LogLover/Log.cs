9fae66c1fe21d496c2ab87a9c3541c6732bfc8a3
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-08-02 16:33:34 +0200

    Log stub for future

---
+++ COSMOS/Assets/Plugins/LogLover/Log.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Log
+{
+    public static void Info(object message)
+    {
+        Message(message, MessageLevel.Info);
+    }
+    public static void Info(object message, params object[] tags)
+    {
+        Message(message, MessageLevel.Info, tags);
+    }
+    public static void Debug(object message)
+    {
+        Message(message, MessageLevel.Debug);
+    }
+    public static void Debug(object message, params object[] tags)
+    {
+        Message(message, MessageLevel.Debug, tags);
+    }
+    public static void Warning(object message)
+    {
+        Message(message, MessageLevel.Warning);
+    }
+    public static void Warning(object message, params object[] tags)
+    {
+        Message(message, MessageLevel.Warning, tags);
+    }
+    public static void Error(object message)
+    {
+        Message(message, MessageLevel.Error);
+    }
+    public static void Error(object message, params object[] tags)
+    {
+        Message(message, MessageLevel.Error, tags);
+    }
+    public static void Fatal(object message)
+    {
+        Message(message, MessageLevel.Fatal);
+    }
+    public static void Fatal(object message, params object[] tags)
+    {
+        Message(message, MessageLevel.Fatal, tags);
+    }
+    public static void Message(object message, MessageLevel level, params object[] tags)
+    {
+        LogLover.Message(message, level, tags);
+    }
+}
+public enum MessageLevel
+{
+    Info,
+    Debug,
+    Warning,
+    Error,
+    Fatal
+}
---
+++ COSMOS/Assets/Plugins/LogLover/LogLover.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System;
+
+public static class LogLover
+{
+    struct LogMessage
+    {
+        public const string Format = "[{4}][{1}][{2}] - {0}\n{3}";
+        public string Message;
+        public MessageLevel Level;
+        public long Time;
+        public string[] Tags;
+        public string[] StackTrace;
+
+        public override string ToString()
+        {
+            return string.Format(Format,
+                Message,
+                Level,
+                string.Join(",", Tags),
+                string.Join("\n", StackTrace),
+                DateTime.FromFileTimeUtc(Time).ToString("dd.MM.yyyy HH:mm:ss:ffff"));
+        }
+    }
+    readonly static ConcurrentQueue<LogMessage> Logs = new ConcurrentQueue<LogMessage>();
+
+    public static void Message(object message, MessageLevel level, params object[] tags)
+    {
+        LogMessage log = new LogMessage();
+        log.Message = message.ToString();
+        log.Level = level;
+        log.Time = System.DateTime.UtcNow.ToFileTimeUtc();
+        switch (level)
+        {
+            case MessageLevel.Info:
+                log.StackTrace = new string[0];
+                break;
+            case MessageLevel.Debug:
+            case MessageLevel.Warning:
+            case MessageLevel.Error:
+            case MessageLevel.Fatal:
+#if DEBUG
+                StackTrace st = new StackTrace(true);
+#else
+                StackTrace st = new StackTrace();
+#endif
+                var frames = st.GetFrames();
+                log.StackTrace = new string[frames.Length];
+                for (int i = 0; i < frames.Length; i++)
+                {
+                    var frame = frames[i];
+                    var sb = new System.Text.StringBuilder();
+                    sb.Append(frame.GetMethod().ReflectedType.ToString());
+                    sb.Append(":");
+                    sb.Append(frame.GetMethod().ToString());
+#if DEBUG
+                    sb.Append("\n - ");
+                    sb.Append(frame.GetFileName());
+                    sb.Append(" ");
+                    sb.Append(frame.GetFileLineNumber());
+#endif
+                    log.StackTrace[i] = sb.ToString();
+                }
+                break;
+        }
+        if (tags != null)
+        {
+            log.Tags = new string[tags.Length];
+            for (int i = 0; i < tags.Length; i++)
+            {
+                log.Tags[i] = tags[i].ToString();
+            }
+        }
+        else
+        {
+            log.Tags = new string[0];
+        }
+        UnityEngine.Debug.Log(log.ToString());
+        //Logs.Enqueue(log);
+    }
+
+
+
+}
