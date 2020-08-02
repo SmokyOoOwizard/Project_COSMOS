e8fed45385b1de789dd5d16749f0de1b02db2c85
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-08-03 00:54:14 +0200

    Added VectorExtantion

---
+++ COSMOS/Assets/Scripts/Core/VectorExtantion.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace COSMOS.Core
+{
+    public static class VectorExtantion
+    {
+        public static Vector2 ToUnity(this System.Numerics.Vector2 vector)
+        {
+            return new Vector2(vector.X, vector.Y);
+        }
+        public static System.Numerics.Vector2 ToNumerics(this Vector2 vector)
+        {
+            return new System.Numerics.Vector2(vector.x, vector.y);
+        }
+        public static Vector3 ToV3(this Vector2 vector)
+        {
+            return new Vector3(vector.x, 0, vector.y);
+        }
+        public static Vector2 ToV2(this Vector3 vector)
+        {
+            return new Vector2(vector.x, vector.z);
+        }
+    }
+}
