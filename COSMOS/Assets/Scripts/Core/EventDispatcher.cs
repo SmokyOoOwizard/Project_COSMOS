5264bb70d2035b0568279561dad6c27a4b5df79a
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-08-03 01:09:11 +0200

    Added enum EventDispatcher

--- COSMOS/Assets/Scripts/Core/EventDispatcher.cs
+++ COSMOS/Assets/Scripts/Core/EventDispatcher.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace COSMOS.Core
 {
+	public delegate void EventDispatcherDelegate(object evtData);
     public class EventDispatcher : IEventDispatcher
     {
 		private Dictionary<string, List<EventDispatcherDelegate>> listeners = new Dictionary<string, List<EventDispatcherDelegate>>();
@@ -49,12 +50,60 @@ namespace COSMOS.Core
 			}
 		}
 	}
-    public delegate void EventDispatcherDelegate(object evtData);
-    public interface IEventDispatcher
-    {
+	public class EventDispatcher<E> : EventDispatcher, IEventDispatcher<E> where E : Enum
+	{
+		private Dictionary<E, List<EventDispatcherDelegate>> listeners = new Dictionary<E, List<EventDispatcherDelegate>>();
+
+		public void AddListener(E evt, EventDispatcherDelegate callback)
+		{
+			List<EventDispatcherDelegate> evtListeners = null;
+			if (listeners.TryGetValue(evt, out evtListeners))
+			{
+				evtListeners.Remove(callback);
+				evtListeners.Add(callback);
+			}
+			else
+			{
+				evtListeners = new List<EventDispatcherDelegate>();
+				evtListeners.Add(callback);
+
+				listeners.Add(evt, evtListeners);
+			}
+		}
+		public void DropListener(E evt, EventDispatcherDelegate callback)
+		{
+			List<EventDispatcherDelegate> evtListeners = null;
+			if (listeners.TryGetValue(evt, out evtListeners))
+			{
+				for (int i = 0; i < evtListeners.Count; i++)
+				{
+					evtListeners.Remove(callback);
+				}
+			}
+		}
+		public void Dispatch(E evt, object obj)
+		{
+			List<EventDispatcherDelegate> evtListeners = null;
+			if (listeners.TryGetValue(evt, out evtListeners))
+			{
+				for (int i = 0; i < evtListeners.Count; i++)
+				{
+					evtListeners[i](obj);
+				}
+			}
+		}
+
+	}
+	public interface IEventDispatcher
+	{
         void AddListener(string evtName, EventDispatcherDelegate callback);
         void DropListener(string evtName, EventDispatcherDelegate callback);
         void Dispatch(string evtName, object evt);
-
+	}
+    public interface IEventDispatcher<E> : IEventDispatcher where E : Enum
+    {
+		void AddListener(E evt, EventDispatcherDelegate callback);
+		void DropListener(E evt, EventDispatcherDelegate callback);
+		void Dispatch(E evt, object obj);
     }
 }
