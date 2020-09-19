e4fbd1026d83fb75e95de58697543e22a7ddf1d9
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-09-19 16:36:42 +0200

    Change Inventory events for new EventDispatcher

---
+++ COSMOS/Assets/Scripts/Common/Inventory/ItemEventData.cs
@@ -0,0 +1,14 @@
+﻿using COSMOS.Core;
+
+namespace COSMOS.Common.Inventory
+{
+    public struct ItemEventData : IEventData
+    {
+        public Item Item;
+
+        public ItemEventData(Item item)
+        {
+            this.Item = item;
+        }
+    }
+}
---
+++ COSMOS/Assets/Scripts/Common/Inventory/SlotEventData.cs
@@ -0,0 +1,13 @@
+﻿using COSMOS.Core;
+
+namespace COSMOS.Common.Inventory
+{
+    public struct SlotEventData : IEventData
+    {
+        public InventorySlot Slot;
+        public SlotEventData(InventorySlot slot)
+        {
+            this.Slot = slot;
+        }
+    }
+}
