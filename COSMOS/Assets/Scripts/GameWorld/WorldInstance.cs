1fd821bd65bb77dec7783b21c783b2fb6ebd147f
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-09-20 17:35:32 +0200

    Remake world streamer

--- COSMOS/Assets/Scripts/GameWorld/WorldInstance.cs
+++ COSMOS/Assets/Scripts/GameWorld/WorldInstance.cs
@@ -1,9 +1,29 @@
 ﻿using COSMOS.Core;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
 
 namespace COSMOS.GameWorld
 {
     public class WorldInstance
     {
-        public readonly QuadTree<WorldSector> WorldSectors;
+        private readonly HashSet<WorldObject> objects = new HashSet<WorldObject>();
+        private readonly QuadTree<WorldObject> objectsMap = new QuadTree<WorldObject>();
+
+        public bool AddWorldObject(WorldObject worldObject)
+        {
+            if (!objects.Contains(worldObject))
+            {
+                var objectSector = worldObject.Transform.Position;
+                objectsMap.Insert(new Core.Rect(objectSector.GetFullX(), objectSector.GetFullZ(), 0, 0), worldObject);
+                objects.Add(worldObject);
+            }
+            return false;
+        }
+
+        public IReadOnlyQuadTree<WorldObject> GetWorldQuadTree()
+        {
+            return objectsMap as IReadOnlyQuadTree<WorldObject>;
+        }
     }
 }
--- COSMOS/Assets/Scripts/GameWorld/WorldStreamer.cs
+++ COSMOS/Assets/Scripts/GameWorld/WorldStreamer.cs
@@ -14,7 +14,7 @@ namespace COSMOS.GameWorld
 
         public static Vector3I ViewCenter;
 
-        public static WorldInstance WorldToStream;
+        public static WorldInstance WorldToStream { get; private set; }
 
         private static Thread streamThread;
 
@@ -22,19 +22,31 @@ namespace COSMOS.GameWorld
 
         private readonly static AutoResetEvent streamWaitEvent = new AutoResetEvent(false);
 
+        private static List<WorldObject> objects = new List<WorldObject>();
+        private static Dictionary<WorldObject, WorldObjectStreamer> ObjectsStreamers = new Dictionary<WorldObject, WorldObjectStreamer>();
+
+        private static List<WorldObject> newObjects = new List<WorldObject>();
+
         private static bool streamIteraionComplete;
 
-        private static Dictionary<WorldSector, WorldSectorStreamer> controlSectors = new Dictionary<WorldSector, WorldSectorStreamer>();
+
+
+        private static IReadOnlyQuadTree<WorldObject> worldQuadTree;
+
+        public static void SetWorld(WorldInstance world)
+        {
+            worldQuadTree = world.GetWorldQuadTree();
+            WorldToStream = world;
+        }
 
 
         public static void Init()
         {
             threadRun = true;
 
-
+            streamIteraionComplete = true;
             streamThread = new Thread(streamProcess);
             streamThread.Start();
-
         }
         public static void Deinit()
         {
@@ -73,29 +85,39 @@ namespace COSMOS.GameWorld
                     rect.Height = ControlBorder;
                     rect.Width = ControlBorder;
 
-                    var sectors = WorldToStream.WorldSectors.Query(rect);
+                    var toCheck = new List<WorldObject>(objects);
+                    worldQuadTree.Diff(rect, toCheck, newObjects);
+                    foreach (var toRemove in toCheck)
+                    {
+                        objects.Remove(toRemove);
+                        if (ObjectsStreamers.TryGetValue(toRemove, out WorldObjectStreamer streamer))
+                        {
+                            streamer.Free();
+                            ObjectsStreamers.Remove(toRemove);
+                        }
+                    }
+                    foreach (var toAdd in newObjects)
+                    {
+                        objects.Add(toAdd);
+                        if (!ObjectsStreamers.ContainsKey(toAdd))
+                        {
+                            ObjectsStreamers[toAdd] = WorldObjectStreamer.Spawn(toAdd);
+                        }
+                    }
+                    newObjects.Clear();
+                    
+                    updateObjects();
 
-                    foreach (var sector in controlSectors)
-                    {
-                        if (!sectors.Contains(sector.Key))
-                        {
-                            sector.Value.Free();
-                            controlSectors.Remove(sector.Key);
-                        }
-                    }
-                    for (int i = 0; i < sectors.Length; i++)
-                    {
-                        if (!controlSectors.ContainsKey(sectors[i]))
-                        {
-                            var streamer = WorldSectorStreamer.Spawn(sectors[i]);
-                            controlSectors.Add(sectors[i], streamer);
-                            // load new sector
-                        }
-                    }
+
                     // logic
                 }
                 streamIteraionComplete = true;
             }
+        }
+
+        private static void updateObjects()
+        {
+
         }
     }
 }
