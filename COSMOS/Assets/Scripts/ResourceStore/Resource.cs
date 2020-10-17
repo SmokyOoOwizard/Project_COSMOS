99ae14082959fcb27a148e3f494709376f73c98d
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-10-17 15:40:12 +0200

    Added base files for ResourceStore

---
+++ COSMOS/Assets/Scripts/ResourceStore/Resource.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using COSMOS.Core;
+
+namespace COSMOS.ResourceStore
+{
+    public abstract class Resource : EventDispatcher
+    {
+        // ресурс
+        public enum Status
+        {
+            Unloaded,
+            Loaded,
+            Unloading,
+            Loadign
+        }
+
+        public string Name { get; private set; }
+        public string Collection { get; private set; }
+        public bool Internal { get; private set; }
+
+        public Status CurrentStatus { get; private set; }
+
+        private readonly List<ResourceInstance> instances = new List<ResourceInstance>();
+
+        protected void SetCurrentStatus(Status status)
+        {
+            CurrentStatus = status;
+            dispatchEvent(status);
+        }
+
+        public ResourceInstance GetInstance()
+        {
+            var instance = OnGetInstance();
+
+            if(instance != null)
+            {
+                instances.Add(instance);
+            }
+
+            return instance;
+        }
+
+        protected abstract ResourceInstance OnGetInstance();
+
+        internal void ReturnInstance(ResourceInstance instance)
+        {
+
+        }
+    }
+}
---
+++ COSMOS/Assets/Scripts/ResourceStore/ResourceInstance.cs
@@ -0,0 +1,28 @@
+﻿namespace COSMOS.ResourceStore
+{
+    public abstract class ResourceInstance
+    {
+        public Resource Resource { get; private set; }
+
+        public bool IsFree { get; private set; }
+
+        // ссылка на ресурс готовый к работе
+
+        public void Free()
+        {
+            if (IsFree)
+            {
+                return;
+            }
+
+
+            if (Resource != null)
+            {
+                Resource.ReturnInstance(this);
+            }
+            OnFree();
+        }
+
+        protected abstract void OnFree();
+    }
+}
