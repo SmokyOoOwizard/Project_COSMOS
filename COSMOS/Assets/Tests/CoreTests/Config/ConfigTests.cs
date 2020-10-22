959e3eb2e87d0273cbe0c9949c022c3556dc7641
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-10-23 00:26:45 +0200

    Added one test for config parse

---
+++ COSMOS/Assets/Tests/CoreTests/Config/ConfigTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using COSMOS.Core.Config;
+
+namespace ConfigTests
+{
+    public class ConfigTests
+    {
+        [Test]
+        public void ConfigParseTest()
+        {
+            try
+            {
+                IConfigReader reader = XmlConfigReader.CreateReader(
+                        "<TestName>" +
+                        "   <SubConfig Type=\"ConfigType\">" +
+                        "       <SubConfigValue>TestValue</SubConfigValue>" +
+                        "   </SubConfig>" +
+                        "</TestName>");
+
+                var config = Config.ParseConfig(reader);
+
+                Assert.IsNotNull(config);
+                Assert.AreEqual(config.Name, "TestName");
+
+                var subConfig = config["SubConfig"];
+                Assert.IsNotNull(subConfig);
+
+                var subSubConfig = subConfig["SubConfigValue"];
+                Assert.IsNotNull(subSubConfig);
+                Assert.AreEqual(subSubConfig.Value, "TestValue");
+
+                Assert.IsNull(subSubConfig["NullObject"]);
+
+            }
+            catch (System.Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
+        }
+    }
+}
--- COSMOS/Assets/Tests/CoreTests/Config/XmlConfigReaderTests.cs
+++ COSMOS/Assets/Tests/CoreTests/Config/XmlConfigReaderTests.cs
@@ -6,37 +6,40 @@ using UnityEngine.TestTools;
 using COSMOS.Core.Config;
 using COSMOS.Core;
 
-public class XmlConfigReaderTests
+namespace ConfigTests
 {
-    [Test]
-    public void XmlStringParseTest()
+    public class XmlConfigReaderTests
     {
-        try
+        [Test]
+        public void XmlStringParseTest()
         {
-            IConfigReader reader = XmlConfigReader.CreateReader(
-                "<TestName>" +
-                "   <SubConfig Type=\"ConfigType\">" +
-                "       <SubConfigValue>TestValue</SubConfigValue>" +
-                "   </SubConfig>" +
-                "</TestName>");
+            try
+            {
+                IConfigReader reader = XmlConfigReader.CreateReader(
+                    "<TestName>" +
+                    "   <SubConfig Type=\"ConfigType\">" +
+                    "       <SubConfigValue>TestValue</SubConfigValue>" +
+                    "   </SubConfig>" +
+                    "</TestName>");
 
-            Assert.IsNotNull(reader);
-            Assert.AreEqual(reader.Name, "TestName");
-            Assert.IsTrue(reader.HasChildren);
-            Assert.NotZero(reader.ChildCount);
+                Assert.IsNotNull(reader);
+                Assert.AreEqual(reader.Name, "TestName");
+                Assert.IsTrue(reader.HasChildren);
+                Assert.NotZero(reader.ChildCount);
 
-            var firstChild = reader.GetChild(0);
-            Assert.IsNotNull(firstChild);
-            Assert.AreEqual(firstChild.Name, "SubConfig");
-            Assert.AreEqual(firstChild.Type, "ConfigType");
+                var firstChild = reader.GetChild(0);
+                Assert.IsNotNull(firstChild);
+                Assert.AreEqual(firstChild.Name, "SubConfig");
+                Assert.AreEqual(firstChild.Type, "ConfigType");
 
-            var firstChildOfChild = firstChild.GetChild(0);
-            Assert.IsNotNull(firstChildOfChild);
-            Assert.AreEqual(firstChildOfChild.Value, "TestValue");
-        }
-        catch (System.Exception ex)
-        {
-            Assert.Fail(ex.ToString());
+                var firstChildOfChild = firstChild.GetChild(0);
+                Assert.IsNotNull(firstChildOfChild);
+                Assert.AreEqual(firstChildOfChild.Value, "TestValue");
+            }
+            catch (System.Exception ex)
+            {
+                Assert.Fail(ex.ToString());
+            }
         }
     }
 }
