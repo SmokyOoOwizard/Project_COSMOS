380d4c49b0ae9fd9e36d1f3846bd622892f0a5cd
smokyOoOwizard <smoky-santa-claus@yandex.ru>
2020-10-25 23:11:27 +0200

    Base ReflectionBag architecture solution

---
+++ COSMOS/Assets/Scripts/Core/ReflectionsKeeper/MemberReflectionInfo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace COSMOS.Core
+{
+    public abstract class MemberReflectionInfo
+    {
+        protected readonly List<Attribute> attributes = new List<Attribute>();
+
+        internal MemberReflectionInfo()
+        {
+            
+        }
+        public List<Attribute> GetAllAttributesByCondition<T>(Func<T, bool> condition) where T : Attribute
+        {
+            var result = new List<Attribute>();
+
+            if (condition != null)
+            {
+                foreach (var refl in attributes)
+                {
+                    if (refl is T)
+                    {
+                        if (condition(refl as T))
+                        {
+                            result.Add(refl);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public bool ContaintsAttribute<T>() where T : Attribute
+        {
+            return attributes.Any((a) => a is T);
+        }
+        public bool ContaintsAttribute<T>(Func<T, bool> condition) where T : Attribute
+        {
+            if (condition != null)
+            {
+                return attributes.Any((a) => a is T && condition(a as T));
+            }
+            return false;
+        }
+
+    }
+}
---
+++ COSMOS/Assets/Scripts/Core/ReflectionsKeeper/PropertyReflection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace COSMOS.Core
+{
+    public class PropertyReflection : MemberReflectionInfo
+    {
+        internal PropertyReflection(PropertyInfo prop) : base()
+        {
+            var atts = prop.GetCustomAttributes(true);
+            foreach (var att in atts)
+            {
+                attributes.Add(att as Attribute);
+            }
+        }
+    }
+}
